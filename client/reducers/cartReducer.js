01c4ad3d7ad555316a3af704c0b5ac41fd7254f4
Aileen CM <aileenchany@gmail.com>
2022-04-26 20:51:34 -0400

    Modified fetch routes

--- client/reducers/cartReducer.js
+++ client/reducers/cartReducer.js
@@ -4,3 +4,13 @@ const initialState = {
   cartList: [],
   cartTotal: 0,
 };
+
+const cartReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.ADD_TO_CART: {
+      return {
+        ...state,
+      };
+    }
+  }
+};
