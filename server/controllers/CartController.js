5e3ee141141a7467a441a07856f5a60d0b7aa1a3
Aileen CM <aileenchany@gmail.com>
2022-04-27 12:36:34 -0400

    Minor file change

--- server/controllers/CartController.js
+++ server/controllers/CartController.js
@@ -3,7 +3,6 @@ const db = require('../models/models');
 const cartController = {};
 
 cartController.addToCart = (req, res, next) => {
-<<<<<<< HEAD
   console.log('req.body: ', req.body);
   const { userID, knife_id, quantity } = req.body;
   knife_id_num = Number(knife_id);
@@ -23,44 +22,23 @@ cartController.addToCart = (req, res, next) => {
         message: { err: err },
       })
     );
-=======
-	console.log('req.body: ', req.body);
-	const { userID, knife_id, quantity } = req.body;
-	knife_id = Number(knife_id);
-	db.query('INSERT INTO cart VALUES (DEFAULT, $1, $2, $3) RETURNING *', [
-		knife_id,
-		quantity,
-		userID,
-	])
-		.then((data) => {
-			console.log('DATA:  ', data);
-			res.locals.addedItem = data.rows[0];
-			return next();
-		})
-		.catch((err) =>
-			next({
-				log: 'cartController.addToCart',
-				message: { err: err },
-			})
-		);
->>>>>>> main
 };
 
 cartController.getCart = (req, res, next) => {
-	const { id } = req.params;
-	const queryStr = 'SELECT * FROM carts WHERE customer_id = $1';
-	db.query(queryStr, [id])
-		.then((data) => {
-			console.log(data);
-			res.locals.cart = data.rows;
-			next();
-		})
-		.catch((err) =>
-			next({
-				log: 'cartController.getCart',
-				message: { err: err },
-			})
-		);
+  const { id } = req.params;
+  const queryStr = 'SELECT * FROM carts WHERE customer_id = $1';
+  db.query(queryStr, [id])
+    .then((data) => {
+      console.log(data);
+      res.locals.cart = data.rows;
+      next();
+    })
+    .catch((err) =>
+      next({
+        log: 'cartController.getCart',
+        message: { err: err },
+      })
+    );
 };
 
 module.exports = cartController;
