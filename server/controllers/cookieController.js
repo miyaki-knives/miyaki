38a4cdb85fd1fc08dde521f2e367a3ac6cc04ee9
Nat <nat@Nats-MacBook-Pro.local>
2022-04-27 20:29:09 -0400

    merging with upstream changes

---
+++ server/controllers/cookieController.js
@@ -0,0 +1,37 @@
+const db = require('../models/models');
+
+const cookieController = {};
+
+cookieController.setCookie = (req, res, next) => {
+	// find user data in db
+	const queryStr = 'SELECT * FROM customers WHERE username = $1';
+	const { user } = req.body;
+
+	// find userid and store in a cookie
+	db.query(queryStr, [user])
+		.then((data) => {
+			res.cookie(`${user}`, data.rows[0].customer_id, {
+				// 24 hours
+				maxAge: 1000 * 60 * 60 * 24,
+				secure: true,
+				httpOnly: true,
+			});
+			console.log('cookie was created');
+			// console.log(data.rows[0].customer_id);
+			// console.log('Cookies: ', req.cookies);
+			// does not show console logs when making requests in postman - why???
+			return next();
+		})
+		.catch((err) =>
+			next({
+				log: 'cookieController.setCookie',
+				message: { err: err },
+			})
+		);
+};
+
+// check if cookie exists, if it does send that cookie exists in response
+// have frontend fetch this response and render page appropriately
+cookieController.confirmCookie = (req, res, next) => {};
+
+module.exports = cookieController;
